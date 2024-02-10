fa715f58af74c3d9534029ebf8b1161f5a1c76e3
PavlynaKyryk1207 <pavlyna.kyryk12@gmail.com>
2024-02-10 13:26:20 +0000

    pkyryk/export-import

---
+++ jsBasics/task3.js
@@ -0,0 +1,16 @@
+let secondarySchoolAge = 12 
+let PupilAge = +11
+
+console.log(secondarySchoolAge == PupilAge)
+
+let secondarySchoolAge2 = 12
+let PupilAge2 = +12
+console.log (secondarySchoolAge2 === PupilAge2)
+
+let PrimaryAge = 4
+let Pupil = 3.5
+console.log (PrimaryAge < Pupil)
+
+let PrimaryAge1 = 4
+let Pupil1 = 3.5
+console.log (PrimaryAge > Pupil)
