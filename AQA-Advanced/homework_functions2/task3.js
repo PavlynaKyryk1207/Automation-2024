f3bb6a3ebef41712260df381e7347ab42612c125
PavlynaKyryk1207 <pavlyna.kyryk12@gmail.com>
2024-02-21 19:56:23 +0000

    AQA-Advanced/homework_functions2/task3.js

--- AQA-Advanced/homework_functions2/task3.js
+++ AQA-Advanced/homework_functions2/task3.js
@@ -3,13 +3,20 @@ function divide(numerator, denominator){
         if (isNaN(numerator) || isNaN(denominator)){
             throw new Error('Both numerator and denominator must be numbers');
         }
-    } (denominator === 0);{
+    else (denominator === 0);{
         throw new Error('Denominator cannot be zero');
-    }return numerator / denominator; 
-}
-    catch(error) {
+        
+    }
+
+} 
+catch(error) {
         console.log('Error:', error.message);
+        return numerator / denominator;
+
     }finally{
         console.log('Work completed');
     }
 }
+console.log(divide(10, 2));
+console.log(divide(4, 0));
+console.log(divide('abc', 2));
